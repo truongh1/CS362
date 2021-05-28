6dcfb7edecb57c304104f7f728606ee1588d9cda
Hao Truong <truongh@oregonstate.edu>
2021-05-28 11:19:45 -0700

    fixed fizzBuzz.py

--- HW7/fizzBuzz.py
+++ HW7/fizzBuzz.py
@@ -1,13 +1,20 @@
 def fizzBuzz(num):
+    tmp = ""
+    if (num < 0 or num > 100):
+        raise Exception("Number must be between 0 and 100")
     if (num % 3 == 0 and num % 5 == 0):
-        print("FizzBuzz")
+        tmp += "FizzBuzz"
+        print(tmp)
     else:
         if (num % 3 == 0):
-            print("Fizz")
+            tmp += "Fizz"
+            print(tmp)
         if (num % 5 == 0):
-            print("Buzz")
+            tmp += "Buzz"
+            print(tmp)
     if (num % 3 != 0 and num % 5 != 0):
-        print(num)
+        tmp += str(num)
+        print(tmp)
+    return tmp
 
 fizzBuzz(15)
-    
