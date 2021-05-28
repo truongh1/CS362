8fe9568e2284ca671fe4b27a3bd5594b3947d90e
Hao Truong <truongh@oregonstate.edu>
2021-05-28 10:59:17 -0700

    fixed fizzBuzz code

--- HW7/fizzBuzz.py
+++ HW7/fizzBuzz.py
@@ -1,12 +1,13 @@
 def fizzBuzz(num):
     if (num % 3 == 0 and num % 5 == 0):
         print("FizzBuzz")
-    if (num % 3 == 0):
-        print("Fizz")
-    if (num % 5 == 0):
-        print("Buzz")
+    else:
+        if (num % 3 == 0):
+            print("Fizz")
+        if (num % 5 == 0):
+            print("Buzz")
     if (num % 3 != 0 and num % 5 != 0):
         print(num)
 
-
+fizzBuzz(15)
     
