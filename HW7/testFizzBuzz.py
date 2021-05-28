6e6d47ea3709619808ca984ce58a1f9ac9e294eb
Hao Truong <truongh@oregonstate.edu>
2021-05-28 10:46:52 -0700

    wrote test for fizzBuzz

---
+++ HW7/testFizzBuzz.py
@@ -0,0 +1,19 @@
+import unittest
+import fizzBuzz
+
+class TestFizzBuzz(unittest.TestCase):
+
+    def test_case1(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(4), "Fizz")
+
+    def test_case2(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(3), "Fizz")
+
+    def test_case2(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(20), "Buzz")
+
+    def test_case2(self):
+        self.assertEqual(fizzBuzz.fizzBuzz(15), "FizzBuzz")
+
+if __name__ == '__main__':
+    unittest.main()
