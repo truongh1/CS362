55bd74de123ee71d769a016e502280f566fdfd95
Hao Truong <truongh@oregonstate.edu>
2021-05-28 11:21:45 -0700

    test and code for fizzBuzz done

--- HW7/testFizzBuzz.py
+++ HW7/testFizzBuzz.py
@@ -4,15 +4,15 @@ import fizzBuzz
 class TestFizzBuzz(unittest.TestCase):
 
     def test_case1(self):
-        self.assertEqual(fizzBuzz.fizzBuzz(4), "Fizz")
+        self.assertEqual(fizzBuzz.fizzBuzz(4), '4')
 
     def test_case2(self):
         self.assertEqual(fizzBuzz.fizzBuzz(3), "Fizz")
 
-    def test_case2(self):
+    def test_case3(self):
         self.assertEqual(fizzBuzz.fizzBuzz(20), "Buzz")
 
-    def test_case2(self):
+    def test_case4(self):
         self.assertEqual(fizzBuzz.fizzBuzz(15), "FizzBuzz")
 
 if __name__ == '__main__':
